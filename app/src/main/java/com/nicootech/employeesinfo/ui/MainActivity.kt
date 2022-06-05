de0ffcf552716973ff9360274236baee98552c05
Ethan Rahimian <nicootech@gmail.com>
2022-06-05 11:45:02 -0700

    1-init

---
+++ app/src/main/java/com/nicootech/employeesinfo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nicootech.employeesinfo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nicootech.employeesinfo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
